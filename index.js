d3603f43359420abf3ccccf636255f5f4145ce95
cuacua16 <agborrelli@hotmail.com>
2022-10-23 11:14:08 -0300

    public

---
+++ index.js
@@ -0,0 +1,25 @@
+const resumen = document.getElementById("resumen");
+const borrar = document.getElementById("borrar");
+const total = document.getElementById("total");
+const cantidad = document.getElementById("cantidad");
+const select = document.getElementById("select");
+const formu = document.getElementById("formu");
+
+resumen.addEventListener("click", (e) => {
+  e.preventDefault();
+  let n = parseInt(cantidad.value);
+  switch (select.value) {
+    case "estudiante":
+      total.textContent = `Total a pagar: $${200 * 0.2 * n}`;
+    case "trainee":
+      total.textContent = `Total a pagar: $${200 * 0.5 * n}`;
+    case "junior":
+      total.textContent = `Total a pagar: $${200 * 0.85 * n}`;
+  }
+});
+
+borrar.addEventListener("click", (e) => {
+  e.preventDefault();
+  formu.reset();
+  total.textContent = `Total a pagar: $`;
+});
